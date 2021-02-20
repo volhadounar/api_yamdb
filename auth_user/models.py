02677ea1c36d0445f2d75f8dfdbab28d8287fa01
Volha Dounar <volha.dounar@yandex.by>
2021-02-20 23:55:15 +0300

    fix according pep + readme

--- auth_user/models.py
+++ auth_user/models.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
 
-class CustomUserManager(BaseUserManager):
+class CustomUserManager(UserManager):
     """Менеджер пользователей."""
     def create_user(self, email, password=None, **extra_fields):
         """
@@ -16,6 +17,8 @@ class CustomUserManager(BaseUserManager):
             email=self.normalize_email(email),
             **extra_fields
         )
+        if password:
+            user.password = make_password(password)
         user.save(using=self._db)
         return user
 
@@ -24,6 +27,10 @@ class CustomUserManager(BaseUserManager):
         Создает объект суперпользвателя с указанным адресом
         электронной почты, датой рождения и паролем.
         """
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
         user = self.create_user(
             email,
             password=password,
